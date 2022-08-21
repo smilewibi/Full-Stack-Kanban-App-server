6c5eccee5b81cd30bb4ab6115e3ba8c64381fb7d
ismail <wibidjokam@gmail.com>
2022-08-21 15:29:43 +0700

    Kanban Board Feature

--- src/v1/controllers/section.js
+++ src/v1/controllers/section.js
@@ -5,7 +5,7 @@ exports.create = async (req, res) => {
   const { boardId } = req.params
   try {
     const section = await Section.create({ board: boardId })
-    section._doc.task = []
+    section._doc.tasks = []
     res.status(201).json(section)
   } catch (err) {
     res.status(500).json(err)
@@ -19,20 +19,20 @@ exports.update = async (req, res) => {
       sectionId,
       { $set: req.body }
     )
-    section._doc.task = []
+    section._doc.tasks = []
     res.status(200).json(section)
   } catch (err) {
-    res.status(500).json(err)
+    res.status(500).josn(err)
   }
 }
 
 exports.delete = async (req, res) => {
   const { sectionId } = req.params
-    try {
-      await Task.deleteMany({ section: sectionId })
-      await Section.deleteOne({ _id: sectionId })
-      res.status(200).json('deleted')
-    } catch (err) {
-      res.status(500).json(err)
+  try {
+    await Task.deleteMany({ section: sectionId })
+    await Section.deleteOne({ _id: sectionId })
+    res.status(200).json('deleted')
+  } catch (err) {
+    res.status(500).josn(err)
   }
 }
--- src/v1/models/section.js
+++ src/v1/models/section.js
@@ -3,15 +3,15 @@ const Schema = mongoose.Schema
 const { schemaOptions } = require('./modelOptions')
 
 const sectionSchema = new Schema({
-    board: {
-        type: Schema.Types.ObjectId,
-        ref: 'Board',
-        required: true
-    },
-    title: {
-        type: String,
-        default: ''
-    }
+  board: {
+    type: Schema.Types.ObjectId,
+    ref: 'Board',
+    required: true
+  },
+  title: {
+    type: String,
+    default: ''
+  }
 }, schemaOptions)
 
 module.exports = mongoose.model('Section', sectionSchema)
